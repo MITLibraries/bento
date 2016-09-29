5e274b2e0132ec69b75f2864df0cb1393648224f
Jeremy Prevost <jprevost@mit.edu>
2016-09-29 11:17:53 -0400

    Update deps and readme
    - Removes license badge since Github has a license link now for the
    repo directly.
    - Adds CodeClimate badge

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,8 +17,7 @@ ActiveRecord::Schema.define(version: 20150101010101) do
     t.string   "uid",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
-
-  add_index "users", ["uid"], name: "index_users_on_uid", unique: true
 
 end
