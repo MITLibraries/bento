c19680ae8994d6ad7a9466dc75426f6d8583847a
Matt Bernhardt <mjbernha@mit.edu>
2016-10-05 13:20:33 -0400

    First stab at recording interaction events

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function TrackLinks( element ) {
+  $( element ).click( function( e ) {
+    fields = {
+      hitType: 'event',
+      eventCategory: 'Bento',
+      eventAction: $( this ).parents( '.region' ).data( 'region' ),
+      eventLabel: e.target.text
+    };
+    ga( 'send', fields );
+  });
+}
