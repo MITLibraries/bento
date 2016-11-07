c07885ede3f64aace2770515a71d0d4354b99d11
Jeremy Prevost <jprevost@mit.edu>
2016-11-07 15:57:16 -0500

    Adds tests for normalized EDS data
    Why:
    
    * the code that normalizes EDS article and book content did not
    contain a comprehensive set of tests. The tests here should provide
    better assurance that we are handling edge cases as expected.
    
    This change addresses the need by:
    
    * https://mitlibraries.atlassian.net/browse/DI-93
    
    Merged code this is testing was tracked by:
    
    * https://github.com/MITLibraries/bento/pull/59
    * https://mitlibraries.atlassian.net/browse/DI-72
    * https://mitlibraries.atlassian.net/browse/DI-73

---
+++ test/models/normalize_eds_articles_test.rb
@@ -0,0 +1,92 @@
+require 'test_helper'
+
+class NormalizeEdsArticlesTest < ActiveSupport::TestCase
+  def popcorn_articles
+    VCR.use_cassette('popcorn articles',
+                     allow_playback_repeats: true) do
+      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
+      NormalizeEds.new.to_result(raw_query, 'articles')
+    end
+  end
+
+  test 'normalized articles have expected title' do
+    assert_equal(
+      'History of northern corn leaf blight disease in the',
+      popcorn_articles['results'][0].title.split[0...9].join(' ')
+    )
+  end
+
+  test 'normalized articles have expected year' do
+    assert_equal('2016', popcorn_articles['results'][0].year)
+  end
+
+  test 'normalized articles have expected url eds provided sfx link' do
+    assert_equal(
+      'https://sfx.mit.edu/sfx_local?rfr_id=info%3Asid%2FMIT.BENTO&rft.au=Moreira+Ribeiro%2C+Rodrigo%3Bdo+Amaral+J%C3%BAnior%2C+Ant%C3%B4nio+Teixeira%3BFerreira+Pena%2C+Guilherme%3BVivas%2C+Marcelo%3BNascimento+Kurosawa%2C+Railan%3BAzeredo+Gon%C3%A7alves%2C+Leandro+Sim%C3%B5es&rft.issue=4&rft.jtitle=Acta+Scientiarum%3A+Agronomy&rft.volume=38&rft.year=2016&rft_id=info%3Adoi%2F10.4025%2Factasciagron.v38i4.30573',
+      popcorn_articles['results'][0].url
+    )
+  end
+
+  test 'normalized articles have generated sfx link when not in eds' do
+    assert_equal(
+      'https://sfx.mit.edu/sfx_local?genre=article&isbn=&issn=07335210&title=Journal%20of%20Cereal%20Science&volume=69&issue=&date=20160501&atitle=Sensory%20and%20nutritional%20evaluation%20of%20popcorn%20kernels%20with%20yellow,%20white%20and%20red%20pericarps%20expanded%20in%20different%20ways&aulast=Paraginski,%20Ricardo%20Tadeu&spage=383&sid=EBSCO:ScienceDirect&pid=%3Cauthors%3EParaginski,%20Ricardo%20Tadeu%3C/authors%3E%3Cui%3ES0733521016300753%3C/ui%3E%3Cdate%3E20160501%3C/date%3E%3Cdb%3EScienceDirect%3C/db%3E&rfr_id=info:sid/MIT.BENTO',
+      popcorn_articles['results'][1].url
+    )
+  end
+
+  test 'normalized articles have expected type' do
+    assert_equal('Academic Journal', popcorn_articles['results'][0].type)
+  end
+
+  test 'normalized articles have expected authors' do
+    assert_equal(
+      'Moreira Ribeiro, Rodrigo',
+      popcorn_articles['results'].first.authors.first
+    )
+    assert_equal(6, popcorn_articles['results'][0].authors.count)
+  end
+
+  test 'normalized articles can handle no authors' do
+    VCR.use_cassette('no article authors',
+                     allow_playback_repeats: true) do
+      raw_query = SearchEds.new.search('orange', 'apinoaleph')
+      query = NormalizeEds.new.to_result(raw_query, 'articles')
+      assert_nil(query['results'][0].authors)
+    end
+  end
+
+  test 'normalized articles have expected availability' do
+    skip('need to determine logic to how this will work without misleading')
+  end
+
+  test 'normalized articles have expected citation' do
+    assert_equal('volume 38 issue 4', popcorn_articles['results'][0].citation)
+    assert_equal('volume 69', popcorn_articles['results'][1].citation)
+    assert_equal('volume 6', popcorn_articles['results'][2].citation)
+  end
+
+  test 'normalized articles have expected in' do
+    assert_equal(
+      'Acta Scientiarum: Agronomy',
+      popcorn_articles['results'][0].in
+    )
+    assert_equal('Journal of Cereal Science', popcorn_articles['results'][1].in)
+    assert_equal('Procedia Food Science', popcorn_articles['results'][2].in)
+  end
+
+  test 'normalized articles do not have subjects' do
+    assert_nil(popcorn_articles['results'][0].subjects)
+  end
+
+  test 'normalized articles do not have location' do
+    assert_nil(popcorn_articles['results'][0].location)
+  end
+
+  test 'normalized articles do not have publisher' do
+    assert_nil(popcorn_articles['results'][0].publisher)
+  end
+
+  test 'normalized articles do not have thumbnail' do
+    assert_nil(popcorn_articles['results'][0].thumbnail)
+  end
+end
--- test/models/normalize_eds_test.rb
+++ test/models/normalize_eds_test.rb
@@ -1,61 +1,6 @@
 require 'test_helper'
 
 class NormalizeEdsTest < ActiveSupport::TestCase
-  test 'normalized articles have expected title' do
-    VCR.use_cassette('popcorn articles',
-                     allow_playback_repeats: true) do
-      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
-      query = NormalizeEds.new.to_result(raw_query, 'articles')
-      assert_equal(
-        'History of northern corn leaf blight disease in the',
-        query['results'].first.title.split[0...9].join(' ')
-      )
-    end
-  end
-
-  test 'normalized articles have expected year' do
-    VCR.use_cassette('popcorn articles',
-                     allow_playback_repeats: true) do
-      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
-      query = NormalizeEds.new.to_result(raw_query, 'articles')
-      assert_equal('2016', query['results'].first.year)
-    end
-  end
-
-  test 'normalized articles have expected url' do
-    VCR.use_cassette('popcorn articles',
-                     allow_playback_repeats: true) do
-      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
-      query = NormalizeEds.new.to_result(raw_query, 'articles')
-      assert_equal(
-        'https://sfx.mit.edu/sfx_local?rfr_id=info%3Asid%2FMIT.BENTO&rft.au=Moreira+Ribeiro%2C+Rodrigo%3Bdo+Amaral+J%C3%BAnior%2C+Ant%C3%B4nio+Teixeira%3BFerreira+Pena%2C+Guilherme%3BVivas%2C+Marcelo%3BNascimento+Kurosawa%2C+Railan%3BAzeredo+Gon%C3%A7alves%2C+Leandro+Sim%C3%B5es&rft.issue=4&rft.jtitle=Acta+Scientiarum%3A+Agronomy&rft.volume=38&rft.year=2016&rft_id=info%3Adoi%2F10.4025%2Factasciagron.v38i4.30573',
-        query['results'].first.url
-      )
-    end
-  end
-
-  test 'normalized articles have expected type' do
-    VCR.use_cassette('popcorn articles',
-                     allow_playback_repeats: true) do
-      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
-      query = NormalizeEds.new.to_result(raw_query, 'articles')
-      assert_equal('Academic Journal', query['results'].first.type)
-    end
-  end
-
-  test 'normalized articles have expected authors' do
-    VCR.use_cassette('popcorn articles',
-                     allow_playback_repeats: true) do
-      raw_query = SearchEds.new.search('popcorn', 'apinoaleph')
-      query = NormalizeEds.new.to_result(raw_query, 'articles')
-      assert_equal(
-        'Moreira Ribeiro, Rodrigo',
-        query['results'].first.authors.first
-      )
-      assert_equal(6, query['results'].first.authors.count)
-    end
-  end
-
   test 'searches with no results do not error' do
     VCR.use_cassette('no results',
                      allow_playback_repeats: true) do
