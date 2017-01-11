8b2ffdab5c28bdd76c7e80e30d8ff5398bc73c13
Frances Botsford <frances@switchingprotocols.com>
2017-01-11 16:39:27 -0500

    adjust generic layout class names, adjust box desc, adjust view more page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Whitelist web console
-  config.web_console.whitelisted_ips = '10.0.0.0/8'
 end
