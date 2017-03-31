334f4a8d115dd6aa1e90990fa38fe83d6a104439
Jeremy Prevost <jprevost@mit.edu>
2017-03-31 10:16:20 -0400

    Adds access control to flipflop controller
    What:
    
    * adds shared secret key access to flipflop controller in all
      environments (dev / test were previously always accessible)
    * travis defaults to running `rake`, new way is `bin/rails test`
    
    Why:
    
    * the ability to easily toggle features for staff in PR and staging
      builds where they can confirm their current settings requires some
      form of dashboard and flipflop comes with one so why not use it
    
    How:
    
    * setting `ENV['FLIPFLOP_KEY']` and passing that value like
      `/flipflop?flipflop_key=thatthingyouset` will provide access to the
      dashboard where you can see all possible toggles and easily toggle
      features for your session
    * if you don't set `ENV['FLIPFLOP_KEY']` all access is denied to the
      dashboard but you can still toggle features via the
      `/toggle?feature=yourfeature` route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def flipflop_access_control
+    return if session[:flipflop_user]
+    head :forbidden unless valid_flipflop_key?
+    session[:flipflop_user] = true
+  end
+
   def new_session_path(_scope)
     root_path
+  end
+
+  private
+
+  def valid_flipflop_key?
+    return if params[:flipflop_key].blank?
+    params[:flipflop_key] == ENV['FLIPFLOP_KEY']
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MitBento
   class Application < Rails::Application
     # Replace with a lambda or method name defined in ApplicationController
     # to implement access control for the Flipflop dashboard.
-    config.flipflop.dashboard_access_filter = -> { head :forbidden }
+    config.flipflop.dashboard_access_filter = :flipflop_access_control
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
