7e6f534a48e3616530034446956000d7c1e80f05
Jeremy Prevost <jprevost@mit.edu>
2017-08-24 09:17:12 -0400

    Disable eds-api gem internal cache
    The internal cache was making test cassettes frustrating to create.
    
    It is also a file store cache which is not appropriate to our Heroku
    deploy.
    
    I’ll open separate tickets to cache responses similar to how we do
    elsewhere in the app.

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -9,7 +9,10 @@ class RecordController < ApplicationController
     return redirect_to root_url unless record_source && record_an
     session = EBSCO::EDS::Session.new(user: ENV['EDS_USER_ID'],
                                       pass: ENV['EDS_PASSWORD'],
-                                      profile: ENV['EDS_PROFILE'])
+                                      profile: ENV['EDS_PROFILE'],
+                                      guest: false, # this will be dynamic soon
+                                      org: 'mit',
+                                      use_cache: false)
     @record = session.retrieve(dbid: record_source, an: record_an)
     # Don't use q as the parameter here - that will cause the search form to
     # notice the parameter and prefill the search, which is behavior we *don't*
