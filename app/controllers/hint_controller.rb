e0c60621948878c514d2c8273ebfb8e10d488bba
Matt Bernhardt <mjbernha@mit.edu>
2017-08-30 16:51:19 -0400

    Records existance of hint in google analytics via staccato gem

--- app/controllers/hint_controller.rb
+++ app/controllers/hint_controller.rb
@@ -2,6 +2,10 @@ class HintController < ApplicationController
   def hint
     return if params[:q].blank?
     @hint = Hint.match(params[:q])
+    if (@hint && ENV['GOOGLE_ANALYTICS'])
+      tracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS'], nil, ssl: true)
+      tracker.event(category: 'Bento', action: 'Hint', label: 'Show', value: 1)
+    end
     render layout: false
   end
 end
