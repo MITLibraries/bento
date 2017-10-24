a22ab95dcacf08938b22ed0f6b379032192eb758
Jeremy Prevost <jprevost@mit.edu>
2017-10-24 13:54:15 -0400

    Force http thumbnail images to use https

--- test/helpers/record_helper_test.rb
+++ test/helpers/record_helper_test.rb
@@ -69,4 +69,17 @@ DOC
     link = { url: 'https://loc.gov/toc/because/whynot' }
     refute(relevant_fulltext_link?(link))
   end
+
+  test 'force_https with http url changes to https' do
+    assert_equal('https://example.com/yo', force_https('http://example.com/yo'))
+  end
+
+  test 'force_https with https url' do
+    assert_equal('https://example.com/yo',
+                 force_https('https://example.com/yo'))
+  end
+
+  test 'force_https with no scheme returns input unchanged' do
+    assert_equal('example.com/yo', force_https('example.com/yo'))
+  end
 end
