6ea6702fd9a90149d18bc96db65a0c0f6c22e800
Jeremy Prevost <jprevost@mit.edu>
2017-12-13 10:19:40 -0500

    Scan button bug fixes
    - removes double encoding of passed title and changes to
    `URI.encode_www_form_component`
    - adds `rft.date` to populate year field
    - adds `rft.volume` to populate volume field in the future (changes to
    SFX script will be necessary to get this to pass through to Illiad)
    
    See:
    - https://mitlibraries.atlassian.net/browse/DI-618
    - https://mitlibraries.atlassian.net/browse/DI-619
    - prep for https://mitlibraries.atlassian.net/browse/DI-623

--- app/concerns/sfx_handler.rb
+++ app/concerns/sfx_handler.rb
@@ -1,5 +1,4 @@
 class SFXHandler
-
   # The SFX server will do the best it can with whatever it has, so we will
   # initialize the SFXHandler with whatever data we happen to have. We're not
   # guaranteed to have all (or indeed any) of these objects; Aleph and EDS
@@ -11,13 +10,17 @@ class SFXHandler
                  collection: nil,
                  doc_number: nil,
                  library: nil,
-                 title: nil)
+                 title: nil,
+                 year: nil,
+                 volume: nil)
     @barcode = barcode
     @call_number = call_number
     @collection = collection
     @doc_number = doc_number
     @title = title
     @library = library
+    @year = year
+    @volume = volume
   end
 
   # Use this when you want to request a scan of an object.
@@ -33,12 +36,7 @@ class SFXHandler
   private
 
   def url_constructor(scan: false)
-    encoded_title = ERB::Util.url_encode(@title)
-
-    # Yes, sometimes we double-encode, so that when SFX passes parameters
-    # through its next step they end up as hoped.
     if scan
-      encoded_title = ERB::Util.url_encode(encoded_title)
       analytics = 'BENTO'
     else
       # Things with analytics=BENTO are automatically routed through scan &
@@ -54,15 +52,17 @@ class SFXHandler
     # for request items, but this ends up with scan requests for Technique
     # (the yearbook) being routed to a Polish land use journal. Call number
     # fixes this bug and does not seem to introduce new ones.
-    encoded_call_no = ERB::Util.url_encode(@call_number)
+    encoded_call_no = URI.encode_www_form_component(@call_number)
 
     url_parts = [
       sfx_host.to_s,
       "?sid=ALEPH:#{analytics}",
       "&amp;call_number=#{encoded_call_no}",
       "&amp;barcode=#{@barcode}",
-      "&amp;title=#{encoded_title}",
-      "&amp;location=#{encoded_location}"
+      "&amp;title=#{URI.encode_www_form_component(@title)}",
+      "&amp;location=#{encoded_location}",
+      "&amp;rft.date=#{@year}",
+      "&amp;rft.volume=#{@volume}"
     ]
 
     if @doc_number
@@ -71,10 +71,7 @@ class SFXHandler
       )
     end
 
-    if scan
-      url_parts.push('&amp;genre=journal')
-    end
-
+    url_parts.push('&amp;genre=journal') if scan
     url_parts.join('')
   end
 
@@ -84,7 +81,7 @@ class SFXHandler
                else
                  @library
                end
-    ERB::Util.url_encode(ERB::Util.url_encode(location))
+    URI.encode_www_form_component(location)
   end
 
   def sfx_host
