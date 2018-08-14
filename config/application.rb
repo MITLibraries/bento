e4395a7cba8a2737eff282bb2d572fc42164c442
Jeremy Prevost <jprevost@mit.edu>
2018-08-14 14:48:31 -0400

    Initial rails 5.2 upgrade

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,9 @@ module MitBento
     config.flipflop.dashboard_access_filter = :flipflop_access_control
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Compress text-like assets when serving them. This is mostly just needed
     # because we deploy to Heroku and not behind an app server where you'd
