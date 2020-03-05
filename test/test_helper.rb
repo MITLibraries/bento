ceb539e3245185b938a9bbe98bd8c8b3bf475ece
Adam Jazairi <jazairi@mit.edu>
2020-03-05 16:32:28 -0500

    Fix reference to deprecated library mocha/mini_test.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails/capybara'
-require 'mocha/mini_test'
+require 'mocha/minitest'
 
 VCR.configure do |config|
   config.ignore_localhost = true
