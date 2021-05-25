ef397358daba6b8f565de30fd19530e42ae1f23f
Adam Jazairi <jazairi@mit.edu>
2021-05-25 16:23:59 -0400

    Display record-level real-time availability
    Why these changes are being introduced:
    
    Our original plan was to use the Alma Bib API for RTA, but we are
    considering whether a more lightweight option using the Primo Search API
    is viable. This scaled back version of RTA will tell users whether any
    physical copy is available for a given result.
    
    Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/IMP-2093
    
    How this addresses that need:
    
    * Updates the normalize_primo_books model to pull availability status
    from catalog records and pass it into the result model.
    * Adds availability? method to the result model that determines whether
    any copies of a result are available for checkout.
    * Adds check_holdings? method to the result model, which corresponds
    with partial/unclear availability (e.g., one or more volumes is checked
    out).
    * Updates result view to display availability status.
    
    Side effects of this change:
    
    Minor CSS changes to clean up display of the new data.

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,7 +4,7 @@ class Result
   validates :title, presence: true
   validates :url, presence: true
 
-  attr_accessor :an, :authors, :available_url, :blurb, :check_sfx_url,
+  attr_accessor :an, :authors, :availability_status, :available_url, :blurb, :check_sfx_url,
                 :citation, :date_range, :db_source, :fulltext_links,
                 :get_it_label, :in, :location, :marc_856, :online, :openurl,
                 :physical_description, :publisher, :record_links, :subjects,
@@ -152,5 +152,13 @@ class Result
 
   def alma_record?
     an.present? && an.start_with?('alma') ? true : false
+  end
+
+  def available?
+    self.availability_status.any? { |status| status == 'available' }
+  end
+
+  def check_holdings?
+    self.availability_status.any? { |status| status == 'check_holdings' }
   end
 end
