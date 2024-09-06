b52f607574c2566f8b98883293b4b9c18d9f1dad
jazairi <16103405+jazairi@users.noreply.github.com>
2024-09-06 14:22:36 -0400

    Fix routes

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,6 +1,6 @@
 class TacosController < ApplicationController
   # Calls TACOS API to log search terms and return suggested resources. 
-  def tacos
+  def suggested_resources
     return if params[:q].blank?
 
     @response = Tacos.call(params[:q])
