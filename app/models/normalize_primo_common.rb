9e51fd8702e36132d2f543f05f8cfe06386df283
Jeremy Prevost <JPrevost@users.noreply.github.com>
2025-01-21 15:17:34 -0500

    Improve method name
    Code review identified two method names that were likely to be misleading in the future. This
    updates the newly introduced method to be less similar to the existing method.

--- app/models/normalize_primo_common.rb
+++ app/models/normalize_primo_common.rb
@@ -52,13 +52,15 @@ class NormalizePrimoCommon
     authors.map { |author| author.strip.gsub(/\$\$Q.*$/, '') }
   end
 
+  # author_link constructs a link to Primo as an exact creator search
   def author_link(author)
     [ENV.fetch('MIT_PRIMO_URL', nil), '/discovery/search?query=creator,exact,',
-     clean_author(author), '&tab=', ENV.fetch('PRIMO_MAIN_VIEW_TAB', nil), '&search_scope=all&vid=',
+     encode_author(author), '&tab=', ENV.fetch('PRIMO_MAIN_VIEW_TAB', nil), '&search_scope=all&vid=',
      ENV.fetch('PRIMO_VID', nil)].join
   end
 
-  def clean_author(author)
+  # encode_author ensures author components are URI encoded
+  def encode_author(author)
     URI.encode_uri_component(author)
   end
 
